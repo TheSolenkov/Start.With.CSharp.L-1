91c4c615699b9d5e3c27c0f21beb8cfbcb28247e
инж. Ивайло Соленков <solenkov.ivailo@abv.bg>
2015-09-27 13:05:54 -0700

    Delete bybro.cs

--- bybro.cs
+++
@@ -1,3 +0,0 @@
-﻿1>------ Build started: Project: ConsoleApplication22, Configuration: Debug Any CPU ------
-1>  ConsoleApplication22 -> c:\users\phoenix\documents\visual studio 2013\Projects\ConsoleApplication22\ConsoleApplication22\bin\Debug\ConsoleApplication22.exe
-========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
